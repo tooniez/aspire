befbf88a5ad280e7cf6268ee73f52198073d5899
James Newton-King <james@newtonking.com>
2026-08-25 06:56:51 +0800

    Restore dashboard unsecured endpoint FWLink (#19462)

--- src/Aspire.Dashboard/Components/Layout/MainLayout.razor.cs
+++ src/Aspire.Dashboard/Components/Layout/MainLayout.razor.cs
@@ -160,7 +160,7 @@ public partial class MainLayout : IGlobalKeydownListener, IAsyncDisposable
                     options.Link = new()
                     {
                         Text = Loc[nameof(Resources.Layout.MessageUnsecuredEndpointLink)],
-                        Href = "https://aspire.dev/dashboard/security-considerations/",
+                        Href = "https://aka.ms/aspire/api-endpoint-unsecured",
                         Target = "_blank"
                     };
                     options.Intent = MessageIntent.Warning;
